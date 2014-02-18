5bffd162f392c7b8b0943241f6bc306166e529f9
Phil Wolstenholme <phil@wolstenhol.me>
2014-02-18 16:08:22 +0000

    Be more specific with inserting Save and Approve button

--- update-and-approve-changes-with-ctrl-s.js
+++ update-and-approve-changes-with-ctrl-s.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Update and Approve T4 content by pressing Ctrl-S
 // @namespace  http://wolstenhol.me
-// @version    0.2
+// @version    0.5
 // @description  Quickly publish T4 content
 // @include      */SiteManager?ctfn=content&fnno=*&sid=*&svi=*
 // @copyright  2014+, Phil Wolstenholme
@@ -15,11 +15,11 @@ jQuery(document).keydown(function(event) {
     //19 for Mac Command+S
     if (!( String.fromCharCode(event.which).toLowerCase() == 's' && event.ctrlKey) && !(event.which == 19)) return true;
 	saveAndApprove();
-    event.preventDefault();
-    return false;
+	event.preventDefault();
+	return false;
 });
 
-jQuery( ".tabButtons" ).append( "<div><button id='quickSaveApprove'>Press Ctrl+S or click here to Update and Approve</button></div>" );
+jQuery( "#tabs-2 .tabButtons" ).first().append( "<div><button id='quickSaveApprove'>Press Ctrl+S or click here to Update and Approve</button></div>" );
 
 jQuery( "#quickSaveApprove" ).click(function() {
   saveAndApprove();
