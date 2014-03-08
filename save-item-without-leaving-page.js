9776244d90c3e8e54c50141a7f710c4da7ceb44a
Phil Wolstenholme <phil@wolstenhol.me>
2014-03-08 22:06:07 +0000

    Update save-item-without-leaving-page.js

--- save-item-without-leaving-page.js
+++ save-item-without-leaving-page.js
@@ -1,4 +1,4 @@
-jQuery( "#tabs-2 .tabButtons .tab-button-dd-submenu" ).prepend('<li><a href="javascript:;" id="useANewWindow">Save to new window</a></li>');
+jQuery( "#tabs-2 .tabButtons .tab-button-dd-submenu" ).prepend('<li><a href="#" id="useANewWindow">Save to new window</a></li>');
 
 jQuery( "#useANewWindow" ).click(function( event ) {
 	jQuery( "#contentForm" ).attr( "target", "_blank" );
