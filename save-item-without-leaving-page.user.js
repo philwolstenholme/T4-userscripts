5aa2bd5086c5b0c652cb84cb566ce9ec865b1cf4
Phil Wolstenholme <phil@wolstenhol.me>
2014-03-10 10:23:43 +0000

    Altered filenames
    Add .user suffix to all files so Greasemonkey recognises them as
    userscripts

---
+++ save-item-without-leaving-page.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name       Allow T4 to save items without taking you off your current page
+// @namespace  http://wolstenhol.me
+// @version    0.5
+// @description  The form is submitted to a new window
+// @include      */SiteManager?ctfn=content*
+// @copyright  2014+, Phil Wolstenholme
+// ==/UserScript==
+
+jQuery( "#tabs-2 .tabButtons .tab-button-dd-submenu" ).prepend('<li><a href="javascript:;" id="useANewWindow">Save to new window</a></li>');
+
+jQuery( "#useANewWindow" ).click(function( event ) {
+	jQuery( "#contentForm" ).attr( "target", "_blank" );
+	jQuery( ".tab-button-dd-submenu li a" ).first().click();
+});
