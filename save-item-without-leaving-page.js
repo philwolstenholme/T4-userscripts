728a13da4eb76a5f9100df9164f06a0a161acad5
Phil Wolstenholme <phil@wolstenhol.me>
2014-03-10 10:25:30 +0000

    Deleting files without .user suffix

--- save-item-without-leaving-page.js
+++
@@ -1,6 +0,0 @@
-jQuery( "#tabs-2 .tabButtons .tab-button-dd-submenu" ).prepend('<li><a href="#" id="useANewWindow">Save to new window</a></li>');
-
-jQuery( "#useANewWindow" ).click(function( event ) {
-	jQuery( "#contentForm" ).attr( "target", "_blank" );
-	jQuery( ".tab-button-dd-submenu li a" ).first().click();
-});
