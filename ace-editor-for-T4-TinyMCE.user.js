6ef5339e6d4c12f9d781122f5c7f489f43360740
Phil Wolstenholme <phil@wolstenhol.me>
2014-03-18 12:34:54 +0000

    Bit of commenting

--- ace-editor-for-T4-TinyMCE.user.js
+++ ace-editor-for-T4-TinyMCE.user.js
@@ -1,22 +1,24 @@
 // ==UserScript==
 // @name       ACE editor for T4 TinyMCE
 // @namespace  http://wolstenhol.me
-// @version    0.1
+// @version    0.2
 // @description  Use ACE editor for HTML view within TinyMCE
 // @include      */skin/SM/static/common/tiny_mce/themes/advanced/source_editor.htm
 // @copyright  2014+, Phil Wolstenholme
 // ==/UserScript==
 
-// Load jQuery
-
+// Load jQuery, the HTML editor iframe doesn't have it by default
+// We're using the same (old-ish) version that T4 uses
 var scr  = document.createElement('script'),
 head = document.head || document.getElementsByTagName('head')[0];
 scr.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js';
 scr.async = false;
 head.insertBefore(scr, head.firstChild);
 
-var delay=200
+// Wait 0.25 of a second for jQuery to have loaded...
+var delay=25
 setTimeout(function(){
+    // Load the ACE editor. Then set a callback to apply the editor to the page.
 	jQuery.ajax({
 		url: '//ajaxorg.github.io/ace-builds/src-min-noconflict/ace.js',
 		dataType: "script",
@@ -24,20 +26,23 @@ setTimeout(function(){
 	});
 },delay)
 
+
 function success() {
     
-    if (jQuery('#htmlSource').length === 1) { var textareaTarget = '#htmlSource'; }
-    
+    // target the plain old HTML textarea
+    var textareaTarget = '#htmlSource';
     var textareaWidth = jQuery(textareaTarget).css( "width" )
     var textareaHeight = jQuery(textareaTarget).css( "height" )
-    var textarea = jQuery(textareaTarget).hide();
+    var textarea = jQuery(textareaTarget).fadeOut("fast");
     
+    // create a shiny new ACE editor to replace it
     var d = document.createElement('div');
     jQuery(d).attr( "id", "editor" );
     jQuery(d).css( "width", textareaWidth );
     jQuery(d).css( "height", textareaHeight );
-    jQuery( d ).appendTo( jQuery( textarea ).parent() );
+    jQuery(d).insertBefore( ".mceActionPanel" );
     
+    // ACE editor config
     var editor = ace.edit("editor");
     editor.getSession().setUseWorker(false);
     editor.setTheme("ace/theme/chrome");
@@ -57,4 +62,5 @@ function success() {
         var mode = jQuery('option:selected').attr('value');
         editor.getSession().setMode(mode);
     });
+    
 }
