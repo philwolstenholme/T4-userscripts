ae33c1421a975f2ce7dc00fe240c576ea009b051
Phil Wolstenholme <phil@wolstenhol.me>
2014-04-29 11:42:45 +0100

    Remove target attribute from #contentForm

--- save-item-without-leaving-page.user.js
+++ save-item-without-leaving-page.user.js
@@ -12,6 +12,7 @@ jQuery( "#tabs-2 .tabButtons .tab-button-dd-submenu" ).prepend('<li><a href="jav
 jQuery( "#useANewWindow" ).click(function( event ) {
 	jQuery( "#contentForm" ).attr( "target", "_blank" );
 	jQuery( ".tab-button-dd-submenu li a" ).first().click();
+    jQuery( "#contentForm" ).removeAttr( "target" );
 });
 
 jQuery(document).keydown(function(event) {
