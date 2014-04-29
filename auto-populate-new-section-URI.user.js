8ee6370de8ad4133940709bba470e46d97867653
Phil Wolstenholme <phil@wolstenhol.me>
2014-04-29 11:43:09 +0100

    Less specific inclusion rule

--- auto-populate-new-section-URI.user.js
+++ auto-populate-new-section-URI.user.js
@@ -3,7 +3,7 @@
 // @namespace  http://wolstenhol.me
 // @version    0.4
 // @description  Generates lowercase and SEO friendly URIs (for new sections) as you type, without affecting existing URIs
-// @include     */SiteManager?ctfn=hierarchy&fnno=10*
+// @include      *SiteManager?ctfn=hierarchy&fnno=10*
 // @copyright  2014+, Phil Wolstenholme
 // ==/UserScript==
 
