8576619789c86234574bf288a3fe38fa56caa2c7
Phil Wolstenholme <phil@wolstenhol.me>
2014-09-05 14:34:46 +0100

    Expanded to use ACE editor in T4 content layout
    Use ACE for content layout editing instead of Codemirror

--- ace-editor.user.js
+++ ace-editor.user.js
@@ -4,6 +4,7 @@
 // @version    0.1
 // @description  Use ACE editor when editing raw HTML/CSS/JS/PHP
 // @include      */SiteManager?ctfn=content*
+// @include      */SiteManager?ctfn=template*
 // @copyright  2014+, Phil Wolstenholme
 // ==/UserScript==
 
@@ -18,15 +19,20 @@ function success() {
     if (jQuery('#t4_content_element_CodeBody').length === 1) { var textareaTarget = '#t4_content_element_CodeBody'; }
     if (jQuery('#t4_content_element_PHPcontent').length === 1) { var textareaTarget = '#t4_content_element_PHPcontent'; }
     if (jQuery('#t4_content_element_PHP').length === 1) { var textareaTarget = '#t4_content_element_PHP'; }
-    //if (jQuery('#t4_content_element_HTMLcontent').length === 1) { var textareaTarget = '#t4_content_element_HTMLcontent'; }
+    if (jQuery('#t4CodeTemplateContent').length === 1) { var textareaTarget = '#t4CodeTemplateContent'; }
+    if (jQuery('#t4_content_element_PHP').length === 1) { var textareaTarget = '#t4CodeTemplateContent'; }
     
-    var textareaWidth = jQuery(textareaTarget).css( "width" )
-    var textareaHeight = jQuery(textareaTarget).css( "height" )
+    if(codemirrors[0]) { codemirrors[0].toTextArea(); }
+    
+    jQuery( textareaTarget ).height(jQuery( textareaTarget ).height() * 2);
+    
+    var textareaWidth = jQuery(textareaTarget).parent().css( "width" );
+    var textareaHeight = jQuery(textareaTarget).css( "height" );
     var textarea = jQuery(textareaTarget).hide();
-    
+ 
     var d = document.createElement('div');
     jQuery(d).attr( "id", "editor" );
-    jQuery(d).css( "width", textareaWidth );
+    jQuery(d).css( "width",  textareaWidth );
     jQuery(d).css( "height", textareaHeight );
     jQuery( d ).appendTo( jQuery( textarea ).parent() );
     
@@ -34,6 +40,7 @@ function success() {
     editor.getSession().setUseWorker(false);
     editor.setTheme("ace/theme/chrome");
     editor.setShowPrintMargin(false);
+    editor.getSession().setUseWrapMode(true);
     editor.getSession().setMode("ace/mode/html");
     editor.getSession().setValue(textarea.val());
     editor.getSession().on('change', function(){
