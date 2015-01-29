44e0b321ad2749524ed6a09ae199df3032c4e1a5
Phil Wolstenholme <phil@wolstenhol.me>
2015-01-29 16:19:11 +0000

    Added target attribute to break out of iframe

--- proper-modify-content-links.user.js
+++ proper-modify-content-links.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name       Proper links to edit content rather than relying on onclick="T4_ChangePage(...)"
 // @namespace  http://wolstenhol.me
-// @version    0.3
+// @version    0.1
 // @description  Rather than use a T4 function to take you to an edit/modify content page this creates a link with a href value. The advantage is that these can be opened in new tabs whereas links with the T4_ChangePage method can't
 // @include      */SiteManager?ctfn=hierarchy&fnno=130*
-// @copyright  2015+, Phil Wolstenholme
+// @copyright  2014+, Phil Wolstenholme
 // ==/UserScript==
 
 var sid = jQuery("input[name=sid]").attr("value");
@@ -15,6 +15,6 @@ jQuery('tr[id^="ReOrderable"]').each(function( index ) {
   var editLink = 'https://sitemanager.manchester.ac.uk/terminalfour/SiteManager?ctfn=content&fnno=30&sid=' + sid + '&svi=hierarchy:false:20&cid=' + id;
   var $target = $tr.find("td:nth-child(2) div");
   var contentItemTitle = $target.contents().get(0).nodeValue;
-  var contentItemTitleLink = '<a href="' + editLink + '">' + contentItemTitle + '</a>';
+  var contentItemTitleLink = '<a href="' + editLink + '" target="_parent">' + contentItemTitle + '</a>';
   $target.html( $target.html().replace( contentItemTitle, contentItemTitleLink  ) );
 });
