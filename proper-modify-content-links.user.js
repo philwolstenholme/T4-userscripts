e65c5a0857f496043d693082b1f907785f681efd
Phil Wolstenholme <phil@wolstenhol.me>
2015-02-02 12:51:08 +0000

    Better handling of HTML special characters

--- proper-modify-content-links.user.js
+++ proper-modify-content-links.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Proper links to edit content rather than relying on onclick="T4_ChangePage(...)"
 // @namespace  http://wolstenhol.me
-// @version    0.3
+// @version    0.4
 // @description  Rather than use a T4 function to take you to an edit/modify content page this creates a link with a href value. The advantage is that these can be opened in new tabs whereas links with the T4_ChangePage method can't
 // @include      *SiteManager?ctfn=hierarchy&fnno=130*
 // @copyright  2014+, Phil Wolstenholme
@@ -14,7 +14,8 @@ jQuery('tr[id^="ReOrderable"]').each(function( index ) {
   var id = this.id.slice(-6);
   var editLink = 'SiteManager?ctfn=content&fnno=30&sid=' + sid + '&svi=hierarchy:false:20&cid=' + id;
   var $target = $tr.find("td:nth-child(2) div");
-  var contentItemTitle = $target.contents().get(0).nodeValue;
+  var contentItemTitle = $target.first().html();
+    console.log(contentItemTitle);
   var contentItemTitleLink = '<a href="' + editLink + '" target="_parent">' + contentItemTitle + '</a>';
   $target.html( $target.html().replace( contentItemTitle, contentItemTitleLink  ) );
 });
