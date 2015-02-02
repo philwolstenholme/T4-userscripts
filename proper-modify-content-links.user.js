28cfb8fe5d3900f18bf7a7e5f67592ca598a967b
Phil Wolstenholme <phil@wolstenhol.me>
2015-02-02 10:40:46 +0000

    Less brittle @include rule and also remove absolute URL

--- proper-modify-content-links.user.js
+++ proper-modify-content-links.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name       Proper links to edit content rather than relying on onclick="T4_ChangePage(...)"
 // @namespace  http://wolstenhol.me
-// @version    0.1
+// @version    0.3
 // @description  Rather than use a T4 function to take you to an edit/modify content page this creates a link with a href value. The advantage is that these can be opened in new tabs whereas links with the T4_ChangePage method can't
-// @include      */SiteManager?ctfn=hierarchy&fnno=130*
+// @include      *SiteManager?ctfn=hierarchy&fnno=130*
 // @copyright  2014+, Phil Wolstenholme
 // ==/UserScript==
 
@@ -12,7 +12,7 @@ var sid = jQuery("input[name=sid]").attr("value");
 jQuery('tr[id^="ReOrderable"]').each(function( index ) {
   var $tr = jQuery( this );
   var id = this.id.slice(-6);
-  var editLink = 'https://sitemanager.manchester.ac.uk/terminalfour/SiteManager?ctfn=content&fnno=30&sid=' + sid + '&svi=hierarchy:false:20&cid=' + id;
+  var editLink = 'SiteManager?ctfn=content&fnno=30&sid=' + sid + '&svi=hierarchy:false:20&cid=' + id;
   var $target = $tr.find("td:nth-child(2) div");
   var contentItemTitle = $target.contents().get(0).nodeValue;
   var contentItemTitleLink = '<a href="' + editLink + '" target="_parent">' + contentItemTitle + '</a>';
